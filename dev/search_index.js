5522d1e57468c8a54f45b85ac9c57011de19f8f3
Documenter.jl <documenter@juliadocs.github.io>
2021-08-20 21:01:35 +0000

    build based on 342b9e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CoordinateSystems","category":"page"},{"location":"#CoordinateSystems","page":"Home","title":"CoordinateSystems","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CoordinateSystems.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CoordinateSystems]","category":"page"}]
+}
